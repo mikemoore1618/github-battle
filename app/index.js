b78487b4f7f82f3b4b9d82f31cacc85dace697a7
Mike Moore <mikemoore1618@gmail.com>
2019-05-06 19:22:01 -0700

    update package.json

--- app/index.js
+++ app/index.js
@@ -1,22 +1,18 @@
-const React = require('react')
-const ReactDOM = requite('react-dom')
-require('./index.css')
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./index.css');
 
-//What a component has/can have:
-    //state
-    //lifecycle events
-    //UI
 class App extends React.Component {
-    render() {
-        return (
-            <div>
-                Hello World!
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        Hello World!
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('app')
-)
+  <App />,
+  document.getElementById('app')
+) 
