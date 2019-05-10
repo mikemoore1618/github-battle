a993d0ffa60d6c07563e572095ab076f7390d862
Mike Moore <mikemoore1618@gmail.com>
2019-05-09 21:53:24 -0700

    feat: display all friends znd non freinds in list

--- app/index.js
+++ app/index.js
@@ -1,18 +1,61 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+
 require('./index.css');
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        Hello Mike!!
-      </div>
-    )
+class Users extends React.Component {
+    render() {
+      const friends = this.props.list.filter(function(user) {
+                return user.friend === true
+            });
+      const nonFriends = this.props.list.filter(function(user) {
+                return user.friend !== true
+            });
+      return (
+        <div>
+          <h1>Friends</h1>
+          <ul>
+            {friends.map(function(user) {
+              return <li key={user.name}>{user.name}</li>
+            })}
+          </ul>
+          
+          <hr />
+          
+          <h1> Non Friends </h1>
+          <ul>
+            {nonFriends.map(function(user) {
+              return <li key={user.name}>{user.name}</li>
+            })}
+          </ul>        
+        </div>
+      )
+    }
   }
-}
+  
+  ReactDOM.render(
+    <Users list={[
+      { name: 'Tyler', friend: true },
+      { name: 'Ryan', friend: true },
+      { name: 'Michael', friend: false },
+      { name: 'Mikenzi', friend: false },
+      { name: 'Jessica', friend: true },
+      { name: 'Dan', friend: false } ]} 
+    />,
+    document.getElementById('app')
+  );
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-) 
+// class App extends React.Component {
+    
+//   render() {
+//     return (
+//       <div>
+//         Hello Mike!!
+//       </div>
+//     )
+//   }
+// }
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('app')
+// ) 
